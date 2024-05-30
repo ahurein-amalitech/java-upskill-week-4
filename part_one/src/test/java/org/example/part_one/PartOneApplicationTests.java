10cc2a77312673740ef46840003d47167fe6c540
ahurein-amalitech <ebenezer.ahurein@amalitech.com>
2024-05-30 16:34:52 +0000

    feat: complete part one

---
+++ part_one/src/test/java/org/example/part_one/PartOneApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.part_one;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartOneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
